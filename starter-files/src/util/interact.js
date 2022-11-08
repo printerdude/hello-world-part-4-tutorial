dd0bbd55b6661c18bed8b8ef28fc1d24c5038c74
Brian Yoon <infobyoon@gmail.com>
2022-11-07 19:42:37 -0800

    load contract address and ABI

--- starter-files/src/util/interact.js
+++ starter-files/src/util/interact.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require('../contract-abi.json');
+const contractAddress = '0x576E54e96B1a5c5F5eC52eD0a2AC500D9CF2Eb82';
+
 //export const helloWorldContract;
 
-export const loadCurrentMessage = async () => { 
-  
-};
+export const loadCurrentMessage = async () => {};
 
-export const connectWallet = async () => {
-  
-};
+export const connectWallet = async () => {};
 
-export const getCurrentWalletConnected = async () => {
-  
-};
+export const getCurrentWalletConnected = async () => {};
 
-export const updateMessage = async (address, message) => {
-  
-};
+export const updateMessage = async (address, message) => {};
